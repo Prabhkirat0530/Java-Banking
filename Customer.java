d4e116286f567b9c41d20ceaa679fe350e0d3fb7
Prabhkirat <prabhkirat05@gmail.com>
2021-08-21 22:46:56 +0530

    Banking software

---
+++ Customer.java
@@ -0,0 +1,27 @@
+package ArraysBanking;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialAmount) {
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+        addTraction(initialAmount);
+    }
+
+    public void addTraction(double amount) {
+        this.transactions.add(amount);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+}
